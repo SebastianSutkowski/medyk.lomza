1d0add17522d7606327a722f92201f95762fd9b9
Sebastian Sutkowski <sebastian.sutkowski1@gmail.com>
2020-05-04 13:39:35 +0200

    Updates

---
+++ precache-manifest.be4a5e8ade7fe75db022f8b27161a2dd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782db263beaec1096df75d3ae0494a0f",
+    "url": "/medyk.lomza/index.html"
+  },
+  {
+    "revision": "0db2de97a5d195d6ac32",
+    "url": "/medyk.lomza/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "bac2353261c5318ee093",
+    "url": "/medyk.lomza/static/css/main.f80aa480.chunk.css"
+  },
+  {
+    "revision": "0db2de97a5d195d6ac32",
+    "url": "/medyk.lomza/static/js/2.da17d884.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/medyk.lomza/static/js/2.da17d884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac2353261c5318ee093",
+    "url": "/medyk.lomza/static/js/main.1f708358.chunk.js"
+  },
+  {
+    "revision": "c4f1e3eb76ef993743e1",
+    "url": "/medyk.lomza/static/js/runtime-main.2eff4075.js"
+  },
+  {
+    "revision": "41a1eb7d69830382bc8f2d9419fb99f8",
+    "url": "/medyk.lomza/static/media/crossfit.41a1eb7d.jpg"
+  },
+  {
+    "revision": "44dd9dfcd6a8ff175f460e814b85ff4d",
+    "url": "/medyk.lomza/static/media/dumbell.44dd9dfc.jpg"
+  },
+  {
+    "revision": "a41d95380cda28f1f0a6d77ce8a9997f",
+    "url": "/medyk.lomza/static/media/paweł.a41d9538.png"
+  },
+  {
+    "revision": "75698e9530820c764a949253e17b3697",
+    "url": "/medyk.lomza/static/media/reebok.75698e95.png"
+  },
+  {
+    "revision": "e8e83f8af11565ab3638bc0e855ecf4d",
+    "url": "/medyk.lomza/static/media/taekwondo.e8e83f8a.jpg"
+  },
+  {
+    "revision": "2127e4045ac132ad4d32e2f79035a23e",
+    "url": "/medyk.lomza/static/media/team.2127e404.jpg"
+  }
+]);
